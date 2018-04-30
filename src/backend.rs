91f06370e3181e63cf712bcd0f958fde9b46b423
stratact <stratact1@gmail.com>
2018-04-30 01:35:08 -0700

    Upload csv changes

--- src/backend.rs
+++ src/backend.rs
@@ -1,9 +1,10 @@
+extern crate csv;
 extern crate rusqlite;
 
 use self::rusqlite::Connection;
 
-use std::collections::HashMap;
 use std::fs;
+use std::io::prelude::*;
 use std::process::Command;
 use std::thread;
 
@@ -18,23 +19,24 @@ impl PortixConnection for Connection {
                       category            TEXT,
                       package             TEXT,
                       versions            TEXT,
-                      installed_version   TEXT,
-                      recommended_version TEXT,
                       description         TEXT
                       )", &[]).unwrap();
         self.execute("CREATE TABLE installed_packages (
                       category            TEXT,
                       package             TEXT,
-                      versions            TEXT,
-                      installed_version   TEXT,
-                      recommended_version TEXT,
-                      description         TEXT
+                      installed_version   TEXT
+                      )", &[]).unwrap();
+        self.execute("CREATE TABLE recommended_packages (
+                      category            TEXT,
+                      package             TEXT,
+                      recommended_version TEXT
                       )", &[]).unwrap();
 
         let child_output = thread::spawn(move || {
-            String::from_utf8(Command::new("sh")
+            String::from_utf8(
+                Command::new("sh")
                     .arg("-c")
-                    .arg(r"NAMEVERSION='<category>/<name> <version> <description>\n' EIX_LIMIT_COMPACT=0 eix -c --format '<availableversions:NAMEVERSION>' --pure-packages")
+                    .arg(r#"NAMEVERSION='<category>=<name>=<version>=<description>\n' EIX_LIMIT_COMPACT=0 eix -c --format '<availableversions:NAMEVERSION>' --pure-packages|sed -e 's|[,\/"]||g' -e 's/=/,/;s/=/,/;s/=/,/'"#)
                     .output()
                     .expect("failed to get eix output")
                     .stdout
@@ -42,136 +44,158 @@ impl PortixConnection for Connection {
         });
 
         let child_installed_version_output = thread::spawn(move || {
-            let installed_version_output = String::from_utf8(Command::new("sh")
+            String::from_utf8(
+                Command::new("sh")
                     .arg("-c")
-                    .arg(r"qlist -ICv|sed -re 's/-([0-9])/ \1/'")
+                    .arg(r#"qlist -ICcv|sed -e "s/\//\,/" -e "s/[\t ]/\,/" -e "s/\:/\,/""#)
                     .output()
                     .expect("failed to get qlist output")
                     .stdout
-                ).expect("qlist output is not UTF-8 compatible");
-            installed_version_output.lines().map(|line| {
-                let item = &line[0..line.find(' ').unwrap()];
-                let version = &line[(line.find(' ').unwrap() + 1)..line.len()];
-                (item.to_string(), version.to_string())
-            }).collect::<HashMap<_, _>>()
+                ).expect("qlist output is not UTF-8 compatible")
         });
 
         let child_recommended_version_output = thread::spawn(move || {
-            let recommended_version_output = String::from_utf8(Command::new("sh")
+            String::from_utf8(
+                Command::new("sh")
                     .arg("-c")
-                    .arg(r"NAMEVERSION='<category>/<name> <version>\n' EIX_LIMIT_COMPACT=0 eix -c --format '<bestversion:NAMEVERSION>' --pure-packages")
+                    .arg(r#"NAMEVERSION='<category>=<name>=<version>\n' EIX_LIMIT_COMPACT=0 eix -c --format '<bestversion:NAMEVERSION>' --pure-packages|sed -e 's|[,\/"]||g' -e 's/=/,/;s/=/,/'"#)
                     .output()
                     .expect("failed to get eix output")
                     .stdout
-                ).expect("eix output is not UTF-8 compatible");
-            recommended_version_output.lines().map(|line| {
-                let item = &line[0..line.find(' ').unwrap()];
-                let version = &line[(line.find(' ').unwrap() + 1)..line.len()];
-                (item.to_string(), version.to_string())
-            }).collect::<HashMap<_, _>>()
+                ).expect("eix output is not UTF-8 compatible")
         });
 
-        let child_global_keywords = thread::spawn(move || {
-            let global_keywords = String::from_utf8(Command::new("sh")
-                    .arg("-c")
-                    .arg(r"emerge --info|grep ACCEPT_KEYWORDS")
-                    .output()
-                    .expect("failed to get emerge output")
-                    .stdout
-                ).expect("emerge output is not UTF-8 compatible");
-            global_keywords[(global_keywords.find("\"").unwrap() + 1)..global_keywords.rfind("\"").unwrap()]
-                .split(' ').map(|s| s.to_string()).collect::<Vec<_>>()
-        });
+        //let child_global_keywords = thread::spawn(move || {
+        //    let global_keywords = String::from_utf8(
+        //        Command::new("sh")
+        //            .arg("-c")
+        //            .arg(r"emerge --info|grep ACCEPT_KEYWORDS")
+        //            .output()
+        //            .expect("failed to get emerge output")
+        //            .stdout
+        //        ).expect("emerge output is not UTF-8 compatible");
+        //    global_keywords[(global_keywords.find("\"").unwrap() + 1)..global_keywords.rfind("\"").unwrap()]
+        //        .split(' ').map(|s| s.to_string()).collect::<Vec<_>>()
+        //});
 
 
-        let child_arch_list = thread::spawn(move || {
-            let arch_list = String::from_utf8(Command::new("sh")
-                    .arg("-c")
-                    .arg(r"cat $(portageq get_repo_path / gentoo)/profiles/arch.list")
-                    .output()
-                    .expect("failed to get portageq output")
-                    .stdout
-                ).expect("portageq output is not UTF-8 compatible");
-            let mut list = Vec::new();
-            for arch in arch_list.lines() {
-                if arch.is_empty() {
-                    break;
-                }
-                list.push(arch.to_string());
-            }
-            list
-        });
+        //let child_arch_list = thread::spawn(move || {
+        //    let arch_list = String::from_utf8(
+        //        Command::new("sh")
+        //            .arg("-c")
+        //            .arg(r"cat $(portageq get_repo_path / gentoo)/profiles/arch.list")
+        //            .output()
+        //            .expect("failed to get portageq output")
+        //            .stdout
+        //        ).expect("portageq output is not UTF-8 compatible");
+        //    let mut list = Vec::new();
+        //    for arch in arch_list.lines() {
+        //        if arch.is_empty() {
+        //            break;
+        //        }
+        //        list.push(arch.to_string());
+        //    }
+        //    list
+        //});
 
 
-        let mut item = String::new();
-        let mut desc = String::new();
-        let mut versions = String::new();
-        let mut output = child_output.join().unwrap();
-        output.push_str("extra line needed to get previous item in iterator\n");
-        let installed_version_output_map = child_installed_version_output.join().unwrap();
-        let recommended_version_output_map = child_recommended_version_output.join().unwrap();
-        let global_keywords = child_global_keywords.join().unwrap();
-        let arch_list = child_arch_list.join().unwrap();
-        for line in output.lines().map(|line| line.to_string()) {
-            let current_item = &line[0..line.find(' ').unwrap()];
-            if current_item == item {
-                let version_with_desc = &line[(line.find(' ').unwrap() + 1)..line.len()];
-                let version = &version_with_desc[0..version_with_desc.find(' ').unwrap()];
-                versions.push_str(&format!("{}\n", version));
-            }
-            else {
-                if !versions.is_empty() {
-                    let (category, pkg) = {
-                        let mut item_split = item.split("/");
-                        (item_split.next().unwrap(), item_split.next().unwrap())
-                    };
-                    let blank = String::new();
-                    let installed_version = installed_version_output_map.get(&item).unwrap_or(&blank);
-                    let mut keyword = String::new();
-                    let recommended_version = recommended_version_output_map.get(&item).unwrap_or({
-                        for global_keyword in global_keywords.iter() {
-                            for arch in arch_list.iter() {
-                                if global_keyword == arch {
-                                    keyword = "Not available".to_string();
-                                    break;
-                                }
-                                else if *global_keyword == format!("~{}", arch) {
-                                    keyword = "Keyworded".to_string();
-                                    break;
-                                }
-                            }
-                        }
-                        &keyword
-                    });
-                    self.execute("INSERT INTO all_packages (category, package, versions, installed_version, recommended_version, description)
-                                  VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-                                  &[&category, &pkg, &versions, &*installed_version, &*recommended_version, &desc]).unwrap();
-                      
-                      
-                    if !installed_version.is_empty() {
-                        self.execute("INSERT INTO installed_packages (category, package, versions, installed_version, recommended_version, description)
-                                      VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-                                      &[&category, &pkg, &versions, &*installed_version, &*recommended_version, &desc]).unwrap();
-                    }
-                }
-                versions.clear();
-                item = line[0..line.find(' ').unwrap()].to_string();
-                let version_with_desc = &line[(line.find(' ').unwrap() + 1)..line.len()];
-                let version = &version_with_desc[0..version_with_desc.find(' ').unwrap()];
-                desc = version_with_desc[(version_with_desc.find(' ').unwrap() + 1)..version_with_desc.len()].to_string();
-                versions.push_str(&format!("{}\n", version));
-            }
+        let output = child_output.join().unwrap();
+        let installed_packages_output = child_installed_version_output.join().unwrap();
+        let recommended_packages_output = child_recommended_version_output.join().unwrap();
+
+        let mut all_packages_csv = fs::OpenOptions::new().read(true).write(true).create(true).open("./target/debug/portix_all_packages.csv").expect("failed to create portix_all_packages.csv file");
+        let mut installed_packages_csv = fs::OpenOptions::new().read(true).write(true).create(true).open("./target/debug/portix_installed_packages.csv").expect("failed to create portix_installed_packages.csv file");
+        let mut recommended_packages_csv = fs::OpenOptions::new().read(true).write(true).create(true).open("./target/debug/portix_recommended_packages.csv").expect("failed to create portix_recommended_packages.csv file");
+        all_packages_csv.write_all(output.as_bytes()).expect("failed to read all packages output into file");
+        installed_packages_csv.write_all(installed_packages_output.as_bytes()).expect("failed to read installed packages output into file");
+        recommended_packages_csv.write_all(recommended_packages_output.as_bytes()).expect("failed to read recommended packages output into file");
+
+        let mut all_packages_reader = csv::ReaderBuilder::new().has_headers(false).from_path("./target/debug/portix_all_packages.csv").unwrap();
+        let mut installed_packages_reader = csv::ReaderBuilder::new().has_headers(false).from_path("./target/debug/portix_installed_packages.csv").unwrap();
+        let mut recommended_packages_reader = csv::ReaderBuilder::new().has_headers(false).from_path("./target/debug/portix_recommended_packages.csv").unwrap();
+
+        for record in all_packages_reader.records() {
+            let record = record.expect("failed to get csv record");
+            self.execute("INSERT INTO all_packages (category, package, versions, description)
+                          VALUES (?1, ?2, ?3, ?4)",
+                          &[&&record[0], &&record[1], &&record[2], &&record[3]]).unwrap();
         }
+        for record in installed_packages_reader.records() {
+            let record = record.expect("failed to get csv record");
+            self.execute("INSERT INTO installed_packages (category, package, installed_version)
+                          VALUES (?1, ?2, ?3)",
+                          &[&&record[0], &&record[1], &&record[2]]).unwrap();
+        }
+        for record in recommended_packages_reader.records() {
+            let record = record.expect("failed to get csv record");
+            self.execute("INSERT INTO recommended_packages (category, package, recommended_version)
+                          VALUES (?1, ?2, ?3)",
+                          &[&&record[0], &&record[1], &&record[2]]).unwrap();
+        }
+        //let global_keywords = child_global_keywords.join().unwrap();
+        //let arch_list = child_arch_list.join().unwrap();
+        //let mut item = String::new();
+        //let mut desc = String::new();
+        //let mut versions = String::new();
+        //output.push_str("extra line needed to get previous item in iterator\n");
+        //for line in output.lines().map(|line| line.to_string()) {
+        //    let current_item = &line[0..line.find(' ').unwrap()];
+        //    if current_item == item {
+        //        let version_with_desc = &line[(line.find(' ').unwrap() + 1)..line.len()];
+        //        let version = &version_with_desc[0..version_with_desc.find(' ').unwrap()];
+        //        versions.push_str(&format!("{}\n", version));
+        //    }
+        //    else {
+        //        if !versions.is_empty() {
+        //            let (category, pkg) = {
+        //                let mut item_split = item.split("/");
+        //                (item_split.next().unwrap(), item_split.next().unwrap())
+        //            };
+        //            let blank = String::new();
+        //            let installed_version = installed_version_output_map.get(&item).unwrap_or(&blank);
+        //            let mut keyword = String::new();
+        //            let recommended_version = recommended_version_output_map.get(&item).unwrap_or({
+        //                for global_keyword in global_keywords.iter() {
+        //                    for arch in arch_list.iter() {
+        //                        if global_keyword == arch {
+        //                            keyword = "Not available".to_string();
+        //                            break;
+        //                        }
+        //                        else if *global_keyword == format!("~{}", arch) {
+        //                            keyword = "Keyworded".to_string();
+        //                            break;
+        //                        }
+        //                    }
+        //                }
+        //                &keyword
+        //            });
+        //            self.execute("INSERT INTO all_packages (category, package, versions, installed_version, recommended_version, description)
+        //                          VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        //                          &[&category, &pkg, &versions, &*installed_version, &*recommended_version, &desc]).unwrap();
+        //              
+        //              
+        //            if !installed_version.is_empty() {
+        //                self.execute("INSERT INTO installed_packages (category, package, versions, installed_version, recommended_version, description)
+        //                              VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        //                              &[&category, &pkg, &versions, &*installed_version, &*recommended_version, &desc]).unwrap();
+        //            }
+        //        }
+        //        versions.clear();
+        //        item = line[0..line.find(' ').unwrap()].to_string();
+        //        let version_with_desc = &line[(line.find(' ').unwrap() + 1)..line.len()];
+        //        let version = &version_with_desc[0..version_with_desc.find(' ').unwrap()];
+        //        desc = version_with_desc[(version_with_desc.find(' ').unwrap() + 1)..version_with_desc.len()].to_string();
+        //        versions.push_str(&format!("{}\n", version));
+        //    }
+        //}
     }
 
     fn parse_for_sets(&self) {
         self.execute("CREATE TABLE portage_sets (
-                      portage_set         TEXT,
-                      category_and_pkg    TEXT,
-                      versions            TEXT,
-                      installed_version   TEXT,
-                      recommended_version TEXT,
-                      description         TEXT
+                      portage_set      TEXT,
+                      category_and_pkg TEXT,
+                      category         TEXT,
+                      package          TEXT
                       )", &[]).unwrap();
         for set in fs::read_dir("/etc/portage/sets").expect("failed to find /etc/portage/sets directory") {
             let set = set.expect("intermittent IO error");
@@ -180,17 +204,17 @@ impl PortixConnection for Connection {
             for line in set_file.lines() {
                 let mut line = line.unwrap();
                 let mut split = line.split('/');
-                let (category, pkg) = {
+                let (category, package) = {
                     (split.next().unwrap(), split.next().unwrap())
                 }; 
                 
-                let mut statement = self.prepare("SELECT category, package, versions, installed_version, recommended_version, description FROM all_packages").expect("sql cannot be converted to a C string");
+                let mut statement = self.prepare("SELECT category, package FROM all_packages").expect("sql cannot be converted to a C string");
                 let mut rows = statement.query(&[]).expect("failed to query database");
                 while let Some(Ok(row)) = rows.next() {
-                    if row.get::<_, String>(0) == category && row.get::<_, String>(1) == pkg {
-                        self.execute("INSERT INTO portage_sets (portage_set, category_and_pkg, versions, installed_version, recommended_version, description)
-                                      VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-                                      &[&set.path().file_name().unwrap().to_str(), &line, &row.get::<_, String>(2), &row.get::<_, String>(3), &row.get::<_, String>(4), &row.get::<_, String>(5)]).unwrap();
+                    if row.get::<_, String>(0) == category && row.get::<_, String>(1) == package {
+                        self.execute("INSERT INTO portage_sets (portage_set, category_and_pkg, category, package)
+                                      VALUES (?1, ?2, ?3, ?4)",
+                                      &[&set.path().file_name().unwrap().to_str(), &line, &category, &package]).unwrap();
                         break;
                     }
                 }
